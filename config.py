1e04fd5affdcb3e631849ba763535c79eef5a690
devmonzer0 <150098012+devmonzer0@users.noreply.github.com>
2023-11-08 20:55:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int("15170853")
 API_HASH = ("33f17f7ccfa43fd779489dca8ab67e34")
 
-BOT_TOKEN = ("6411129825:AAHt1NSgNWq9fwz-hKWMT7bhuoyZ6KMX8BI")
+BOT_TOKEN = ("6611121989:AAGEuI38xh3RP8p-A8rKhOxnrml9QTSU5ew")
 OWNER_ID = int("6085292025")
 
 MONGO_DB_URI = ("mongodb+srv://islam2005:islam2005@cluster0.rdjot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
